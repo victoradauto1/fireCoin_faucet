b4f63052399202dfe2abedaaa715abbebfb49c47
Victor Adauto <victoradauto@yahoo.com.br>
2024-12-11 21:49:09 -0300

    - Refatoração de conteúdo do Example Cover Bootstrap; - Criação da função onBtnClick em App.tsx; - Criação do arquivo Web3Service.ts; - Criação do arquivo react-app-env.d.ts em src; - Criação do arquivo abi.json em src; - Insatlação da lib dotenv; - Criação dos arquivos .env e .env.example na pasta raiz;

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,71 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import {mint} from './Web3Service'
 
 function App() {
+
+  function onBtnClick(){
+    mint()
+      .then((tx)=> alert(tx))
+      .catch( err => alert(err.message) );
+  }
+
   return (
-    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <header className="mb-auto">
-    <div>
-      <h3 className="float-md-start mb-0">Cover</h3>
-      <nav className="nav nav-masthead justify-content-center float-md-end">
-        <a className="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
-        <a className="nav-link fw-bold py-1 px-0" href="#">Features</a>
-        <a className="nav-link fw-bold py-1 px-0" href="#">Contact</a>
-      </nav>
+    <div className="d-flex flex-column min-vh-100 bg-dark text-white">
+      {/* Menu no topo */}
+      <header className="p-3 bg-dark">
+        <div className="container d-flex justify-content-between align-items-center">
+          <h3 className="mb-0">FireCoin Faucet</h3>
+          <nav className="nav nav-masthead">
+            <a
+              className="nav-link fw-bold px-2 text-white active"
+              aria-current="page"
+              href="#"
+            >
+              Home
+            </a>
+            <a className="nav-link fw-bold px-2 text-white" href="#">
+              About
+            </a>
+          </nav>
+        </div>
+      </header>
+
+      {/* Main centralizado verticalmente */}
+      <main className="d-flex flex-grow-1 align-items-center justify-content-center text-center">
+        <div>
+          <h1>Get your FireCoins.</h1>
+          <p className="lead">
+            Once a day, earn 1.000 FireCoins for free, just connecting your
+            Metamask below.
+          </p>
+          <p className="lead">
+            <a
+              href="#"
+              onClick={()=>onBtnClick()}
+              className="btn btn-lg btn-light fw-bold border-white bg-white"
+            >
+              <img src="/assets/MetaMask_Fox.svg.png" alt="MetaMask Logo" width={48} />
+              Connect MetaMask
+            </a>
+          </p>
+        </div>
+      </main>
+
+      {/* Rodapé no final */}
+      <footer className="text-white-50 text-center p-3">
+        <p>
+          Built by {" "}
+          <a href="https://www.linkedin.com/in/victor-silva-073b60267/" className="text-white">
+            Victor Silva
+          </a>
+          , available at {" "}
+          <a href="https://github.com/victoradauto1" className="text-white">
+            @victoradauto1
+          </a>
+          .
+        </p>
+      </footer>
     </div>
-  </header>
-
-  <main className="px-3">
-    <h1>Cover your page.</h1>
-    <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-    <p className="lead">
-      <a href="#" className="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
-    </p>
-  </main>
-
-  <footer className="mt-auto text-white-50">
-    <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
-  </footer>
-</div>
   );
 }
 
